42c489f628ce4bfe5274c57776bab51da2e3a9aa
Alexia Averman <alexia.averman@gmail.com>
2025-08-06 14:25:14 -0400

    Initial Commit

---
+++ lesson10/lesson10App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson10App.swift
+//  lesson10
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct lesson10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
