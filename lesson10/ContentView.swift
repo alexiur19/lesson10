2b0cf4ebcc230abbbd2db4da85941299877098a8
Alexia Averman <alexia.averman@gmail.com>
2025-08-06 14:53:08 -0400

    added @state

--- lesson10/ContentView.swift
+++ lesson10/ContentView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+   @State private var textTitle = "what is your name?"
     var body: some View {
         VStack {
-           Text ("what is your name?")
+            Text ("what is your name?")
                 .font(.title)
+            //            TextFeild("Type your name here...",
+            //                      text:Value)
+            TextField("enter name", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                       Button("Submit Name")
+            texTiles= "welcome", \(name)!"
+            name =**
+            //
+            //
+            //        }
+            Button("Button") {
+                print(name)
+            }
+            .font (.title2)
+            .tint (.purple)
+            
         }
         .padding()
     }
